28a683e51d6f1281237c531f947dc902cbcf42d0
Jade Vance <jadenicolevance@gmail.com>
2016-06-29 14:54:20 -0700

    first pass solution, need to figure out how to set timeout to each button and map button press to keys on keyboard

--- noise.js
+++ noise.js
@@ -1,3 +1,38 @@
+// function instrument(allNotes) { 
+//   this.allNotes = allNotes
+//   // play(note)
+//   this.playC = function() {
+//     var cAudio = $('audio#cAudio')[0].play()
+//   }
+//   this.PlayD = function(){
+
+//   }
+// }
+
+
 $(document).ready( function() {
-  // your code here
+  var buttons = $('button')
+  var allNotes = $('audio')
+
+  buttons.on('click', function(event) {
+    event.preventDefault()
+    var button = $(this)
+    if (button.hasClass('note c')) {
+      var cAudio = $('audio#cAudio')[0].play()
+    } else if (button.hasClass('note d')) {
+      var dAudio = $('audio#dAudio')[0].play()
+    } else if (button.hasClass('note e')) {
+      var eAudio = $('audio#eAudio')[0].play()
+    } else if (button.hasClass('note f')) {
+      var fAudio = $('audio#fAudio')[0].play()
+    } else if (button.hasClass('note g')) {
+      var gAudio = $('audio#gAudio')[0].play()
+    } else if (button.hasClass('note a')) {
+      var aAudio = $('audio#aAudio')[0].play()
+    } else if (button.hasClass('note b')) {
+      var bAudio = $('audio#bAudio')[0].play()
+    } else {
+      console.log('else conditon hit!!')
+    }
+  })
 });
