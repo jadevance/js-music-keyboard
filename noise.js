9d8e37cd47c82bc0fc7ba38394dd7adae73348c7
Jade Vance <jadenicolevance@gmail.com>
2016-06-29 23:05:08 -0700

    wrapped the functions into a constructor function called Keyboard. Need to finish key press next

--- noise.js
+++ noise.js
@@ -1,16 +1,26 @@
-function initialize() {
-  var buttons = $('button.note');
-  $.each(buttons, function(index, button) {
-    var _button = $(button);
-    var note = _button.attr('class').split(' ')[1];
+var Keyboard = function() {
+  var instance = this;
 
-    _button.click(function() {
-      playNote(note);
+  this.initialize = function() {
+    this._setupButtons();
+    this._setupKeyboard();
+  };
+
+  this._setupButtons = function() {
+    $('button.note').click(function() {
+      var note = $(this).attr('class').split(' ')[1];
+      instance._playNote(note);
     });
-  });
+  };
+
+  this._playNote = function (note) {
+    var audioId = '#' + note + 'Audio';
+    $(audioId)[0].play();
+  };
 };
 
 $(document).ready( function() {
-  initialize(); 
+  var keyboard = new Keyboard();
+  keyboard.initialize();
 });
 
