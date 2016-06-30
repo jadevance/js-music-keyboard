8e844646443980c19f4ad8ec2e677b78b077811e
Jade Vance <jadenicolevance@gmail.com>
2016-06-29 23:09:41 -0700

    keyboard works, added ASDFGHJ to map to CDEFGAB(notes).

--- noise.js
+++ noise.js
@@ -16,6 +16,45 @@ var Keyboard = function() {
   this._playNote = function (note) {
     var audioId = '#' + note + 'Audio';
     $(audioId)[0].play();
+    // resets the audiofile to time zero with each event 
+    $(audioId)[0].currentTime = 0 
+  };
+
+  this._setupKeyboard = function() {
+    $(document).keypress(onKeyPress);
+
+    function onKeyPress(event) {
+      switch(event.which) {
+        // a
+        case 97:
+          instance._playNote('c');
+          break;
+        // s  
+        case 115:
+          instance._playNote('d');
+          break;
+        // d
+        case 100:
+          instance._playNote('e');
+          break;
+        // f
+        case 102:
+          instance._playNote('f');
+          break;
+        // g  
+        case 103:
+          instance._playNote('g');
+          break;
+        // h
+        case 104:
+          instance._playNote('a');
+          break;
+        // j 
+        case 106:
+          instance._playNote('b');
+          break;
+      };
+    };
   };
 };
 
