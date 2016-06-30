2105461f79f753eba4e1bc49f8b405888004732b
Jade Vance <jadenicolevance@gmail.com>
2016-06-29 23:02:31 -0700

    extracted out the giant if/else block in the document.ready into its own function

--- noise.js
+++ noise.js
@@ -1,40 +1,16 @@
-// function instrument(allNotes) { 
-//   this.allNotes = allNotes
-//   // play(note)
-//   this.playC = function() {
-//     var cAudio = $('audio#cAudio')[0].play()
-//   }
-//   this.PlayD = function(){
+function initialize() {
+  var buttons = $('button.note');
+  $.each(buttons, function(index, button) {
+    var _button = $(button);
+    var note = _button.attr('class').split(' ')[1];
 
-//   }
-// }
-
+    _button.click(function() {
+      playNote(note);
+    });
+  });
+};
 
 $(document).ready( function() {
-  var buttons = $('button')
-  // var allNotes = $('audio')
-
-  buttons.on('click', function(event) {
-    event.preventDefault()
-    var button = $(this)
-    if (button.hasClass('note c')) {
-      var audio = $('audio#cAudio')[0]
-    } else if (button.hasClass('note d')) {
-      var audio = $('audio#dAudio')[0]
-    } else if (button.hasClass('note e')) {
-      var audio = $('audio#eAudio')[0]
-    } else if (button.hasClass('note f')) {
-      var audio = $('audio#fAudio')[0]
-    } else if (button.hasClass('note g')) {
-      var audio = $('audio#gAudio')[0]
-    } else if (button.hasClass('note a')) {
-      var audio = $('audio#aAudio')[0]
-    } else if (button.hasClass('note b')) {
-      var audio = $('audio#bAudio')[0]
-    } else {
-      console.log('else conditon hit!!')
-    }
-    audio.play();
-    audio.currentTime = 0 
-  })
+  initialize(); 
 });
+
